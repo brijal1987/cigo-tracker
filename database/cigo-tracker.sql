bd4ba08272152d92db451c0e2be0dd1eb2586420
Brijal Savaliya <brijal.savaliya@gmail.com>
2020-02-28 18:20:35 +0530

    changes in map

--- database/cigo-tracker.sql
+++ database/cigo-tracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 25, 2020 at 10:36 AM
+-- Generation Time: Feb 28, 2020 at 12:49 PM
 -- Server version: 5.7.25
 -- PHP Version: 7.3.1
 
@@ -17,74 +17,55 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `migration`
+-- Table structure for table `order`
 --
 
-CREATE TABLE `migration` (
-  `version` varchar(180) NOT NULL,
-  `apply_time` int(11) DEFAULT NULL
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(15) NOT NULL,
+  `order_type_id` int(11) NOT NULL,
+  `order_value` varchar(255) DEFAULT NULL,
+  `schedule_date` date NOT NULL,
+  `street_address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `zip_code` varchar(255) DEFAULT NULL,
+  `country_id` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL COMMENT '1=>Pending,2=>Assigned,3=>On Route,4=>Done,5=>Cancelled',
+  `lat` varchar(255) NOT NULL,
+  `lon` varchar(255) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `migration`
+-- Dumping data for table `order`
 --
 
-INSERT INTO `migration` (`version`, `apply_time`) VALUES
-('m000000_000000_base', 1582622712),
-('m130524_201442_init', 1582622754),
-('m190124_110200_add_verification_token_column_to_user_table', 1582622754);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `status` smallint(6) NOT NULL DEFAULT '10',
-  `created_at` int(11) NOT NULL,
-  `updated_at` int(11) NOT NULL,
-  `verification_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `verification_token`) VALUES
-(1, 'cigotracker', 'nTNbEglyOG5BAfyK_EBc7ODdYWyCmiY0', '$2y$13$dZstkRo0oMmmHcrg8TIRceORFyQ8NVSKDahOP4QXr1c92aL4rQnB2', NULL, 'cigotracker@test.com', 10, 1582622836, 1582622836, 'KOabiUtHkMGdSwNuSb83hyJk4Vgc8e7E_1582622836');
+INSERT INTO `order` (`id`, `first_name`, `last_name`, `email`, `phone`, `order_type_id`, `order_value`, `schedule_date`, `street_address`, `city`, `state`, `zip_code`, `country_id`, `status`, `lat`, `lon`, `created_at`, `updated_at`) VALUES
+(1, 'Brijal', 'Savaliya', 'brijal.savaliya@gmail.com', '989865443', 2, '99.99', '2020-03-06', '1109 N Highland St', 'Arlington', 'VA', '', 2, 1, '38.886672', '-77.094735', 1582891612, 1582891612),
+(2, 'Kyle', 'Howard', 'kyle@test.com', '97766665555', 1, '99.99', '2020-03-07', '525 University Ave', 'Toronto', 'ON', '', 1, 1, '41.905021', '-90.864035', 1582892009, 1582892009),
+(5, 'Sam', 'William', 'brijal.savaliya@gmail.com', '989865443', 1, '99.99', '2020-03-10', '1109 N Highland St', 'Arlington', 'VA', '', 2, 1, '38.886672', '-77.094735', 1582892140, 1582892140);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `migration`
+-- Indexes for table `order`
 --
-ALTER TABLE `migration`
-  ADD PRIMARY KEY (`version`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `order`
 --
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
